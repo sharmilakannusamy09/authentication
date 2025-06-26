b16266770e6166594c97e98592e626476e09b4ae
sharmilakannusamy09 <sharmilakannusamy09@gmail.com>
2025-06-26 12:51:49 +0530

    updated

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -13,7 +13,7 @@ export default function Home() {
   }, []);
 
   const lg = async () => {
-    await axios.post('http://localhost:5000/api/auth/logout', {}, { withCredentials: true });
+    await axios.post('https://authentication-3ynw.onrender.com/api/auth/logout', {}, { withCredentials: true });
     setU(null);
   };
 
